242abb78045ec1d4441aca11e6f9c97fc69f443f
jugglingcats <github@akirkpatrick.com>
2023-11-02 21:03:42 +0000

    chore: update package.json with version on release build

--- build.mjs
+++ build.mjs
@@ -2,7 +2,23 @@ import esbuild from "esbuild"
 import fs from "fs"
 import svgrPlugin from "esbuild-plugin-svgr"
 
+const GITHUB_TAG_PREFIX = "refs/tags/"
+const [, , version_or_github_ref] = process.argv
+
 const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"))
+
+if (version_or_github_ref?.length) {
+    const version = version_or_github_ref.startsWith(GITHUB_TAG_PREFIX)
+        ? version_or_github_ref.substring(GITHUB_TAG_PREFIX.length)
+        : version_or_github_ref
+
+    console.log("Bumping version to", version)
+    pkg.version = version
+    fs.writeFileSync("./package.json", JSON.stringify(pkg, null, 2))
+} else {
+    console.log("No version specified, not bumping version")
+}
+
 const externals = [
     pkg.dependencies,
     pkg.devDependencies,
